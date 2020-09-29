6028e79e5acf6eb48dcc2a9d06c5f40a537de86c
akups <appiah-lindberg@outlook.com>
2020-09-29 22:51:03 +0200

    uuid installed and get function created

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express");
+const { v4 } = require("uuid");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello express:)");
+const shoppingList = [
+  { id: "1", name: "cereal", price: 2 },
+  { id: "2", name: "sugar", price: 1 },
+  { id: "3", name: "beef", price: 5 },
+];
+
+app.get("/shoppingList", (req, res) => {
+  res.json(shoppingList);
 });
 
+app.get("/product:productId", (req, res) => {
+  const product = shoppingList.find(pr, (req, res) => {
+    const body = req.body;
+  });
+});
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
